061f6b025836920d2070649af1f534449b6e52ef
Reinan <62473647+reinanricardo@users.noreply.github.com>
2020-06-04 21:52:44 -0300

    aplicando JS

---
+++ scripts/intro.js
@@ -0,0 +1,50 @@
+// document.write("Hello!")
+
+//   
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande logica de corrar")
+//   }
+// }
+
+
+// pessoa.correr()
+
+
+/*Array- Vetores*/
+
+// const colecaoDeBolinhas = [`blue`, `green`, 1, {name: `My name`}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+/* funções - funcionalidade -atalhos - reuso de código*/
+
+
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName(`Ricardo`)
+// sayMyName(`Joice`)
+// sayMyName(`Ramon`)
+
+/*condicionais */
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write(`Reprovado`)
+// }else {
+//   document.write(`Aprovado`)
+// }
+
+/* loop ou repetições */
+
+// for  (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
