8c7a2e7cb042a5e214fd4fd9a25aca2f3b0ce77e
Reinan <62473647+reinanricardo@users.noreply.github.com>
2020-06-07 14:37:39 -0300

    Tercceira Aula

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -27,12 +27,18 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
+        
 
         for( let city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
 
         }
 
@@ -45,3 +51,51 @@ document
 
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+
+        const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+
+        for (const item of itemsToCollect) {
+            item.addEventListener("click", handleSelectedItem)
+        }
+
+        const collectedItems = document.querySelector("input[name=items")
+
+
+        let selectedItems = []
+
+        function handleSelectedItem(event) {
+            const itemLi = event.target
+
+            // Toggle Adiciona e remove 
+            itemLi.classList.toggle("selected")
+
+            const itemid = itemLi.dataset.id
+
+            // verificar se enxistem itens selecionados, se sim
+            // pegar os itens selecionados
+
+            const alreadySelected = selectedItems.findIndex( item => {
+                const itemFound = item == itemid
+                return itemFound
+            })
+            
+            if ( alreadySelected >= 0 ) {
+                const filteredItems = selectedItems.filter( item => {
+                    const itemisDifferent = item != itemid
+                    return itemisDifferent
+                })
+    
+                selectedItems = filteredItems
+        } else {
+            selectedItems.push(itemid)
+
+        }
+
+        collectedItems.value = selectedItems
+
+
+    
+        }
